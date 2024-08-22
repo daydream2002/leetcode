3ccbf344387c402aef160c42168cf659a55cc9a1
daydream <daydream_wjh@qq.com>
2024-08-22 10:33:48 +0800

    72题 多维动态规划

--- src/No72.java
+++ src/No72.java
@@ -1,50 +1,35 @@
 /**
- * @author wjh
- * @date 2024/7/21 下午7:37
+ * Author daydream
+ * Description
+ * Date 2024/8/22
  */
 public class No72 {
     public static void main(String[] args) {
-        Solution72 solution72 = new Solution72();
-        int[][] matrix = {
-                {1}
-        };
-        boolean i = solution72.searchMatrix(matrix, 2);
-        System.out.println(i);
+        int ans = new Solution72().minDistance("horse", "ros");
+        System.out.println(ans);
     }
-
 }
 
 class Solution72 {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int[] ints = new int[matrix.length];
-        for (int i = 0; i < matrix.length; i++) {
-            ints[i] = matrix[i][0];
+    public int minDistance(String word1, String word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            dp[i][0] = i;
         }
-        int i = binarySearch(ints, target);
-        if (i < matrix.length && ints[i] == target)
-            return true;
-        else {
-            if (i == 0)
-                return false;
-            int j = binarySearch(matrix[i - 1], target);
-            if (matrix[i - 1].length > j && matrix[i - 1][j] == target)
-                return true;
+        for (int j = 1; j <= len2; j++) {
+            dp[0][j] = j;
         }
-        return false;
-    }
-
-    int binarySearch(int[] matrix, int target) {
-        int length = matrix.length;
-        int i = 0, j = length - 1, k = length / 2;
-        while (i <= j) {
-            if (target == matrix[k])
-                return k;
-            if (target > matrix[k])
-                i = k + 1;
-            if (target < matrix[k])
-                j = k - 1;
-            k = (i + j) / 2;
+        for (int i = 1; i <= len1; i++) {
+            char a = word1.charAt(i - 1);
+            for (int j = 1; j <= len2; j++) {
+                char b = word2.charAt(j - 1);
+                if (a == b)
+                    dp[i][j] = Math.min(dp[i][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i - 1][j - 1]));
+                else
+                    dp[i][j] = Math.min(dp[i][j - 1] + 1, Math.min(dp[i - 1][j] + 1, dp[i - 1][j - 1] + 1));
+            }
         }
-        return i;
+        return dp[len1][len2];
     }
 }
