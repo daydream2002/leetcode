ef87334434d8bcb8a323b26ad51153efa733c51e
daydream <daydream_wjh@qq.com>
2024-09-23 17:40:51 +0800

    228题,汇总区间

--- src/No228.java
+++ src/No228.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -5,17 +6,38 @@ import java.util.List;
  * @date 2024/9/22 19:00
  */
 public class No228 {
+    public static void main(String[] args) {
+        List<String> strings = new Solution228().summaryRanges(new int[]{1, 3});
+    }
 }
 
 class Solution228 {
     public List<String> summaryRanges(int[] nums) {
-        int l = 0, r = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1] + 1)
-                r = r + 1;
-            else{
 
+        int l = 0, r = 0;
+        ArrayList<String> list = new ArrayList<>();
+        if (nums.length == 0)
+            return list;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                r = i;
+            } else {
+                if (l == r) {
+                    list.add(nums[r] + "");
+                    l = l + 1;
+                    r = l;
+                } else {
+                    list.add(nums[l] + "->" + nums[r]);
+                    r = i;
+                    l = i;
+                }
             }
         }
+        if (l == r)
+            list.add(nums[r] + "");
+        else
+            list.add(nums[l] + "->" + nums[r]);
+        System.out.println(list);
+        return list;
     }
 }
