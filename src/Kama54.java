caaa0e931758844b220afd0e818c2ec3874ec16b
daydream <daydream_wjh@qq.com>
2024-10-29 22:16:00 +0800

    10月29

---
+++ src/Kama54.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Author daydream
+ * Description
+ * Date 2024/10/29 11:44
+ */
+public class Kama54 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String string = sc.next();
+        char[] charArray = string.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (char c : charArray) {
+            if ('0' <= c && c <= '9')
+                sb.append("number");
+            else
+                sb.append(c);
+        }
+        System.out.println(sb.toString());
+    }
+}
