190b62f0956cd64a4c70665a9fdfcc4ed5c191fa
Wang Jiahao <142191394+daydream2002@users.noreply.github.com>
2024-12-23 23:26:01 +0800

    Update No188.java

--- src/No188.java
+++ src/No188.java
@@ -12,16 +12,18 @@ public class No188 {
     }
 }
 
-class Solution188 {
+class Solution188{
     public int maxProfit(int k, int[] prices) {
         int[][] dp = new int[prices.length][2 * k + 1];
-        for (int i = 0; i < prices.length; i++) {
+        for (int j = 0; j < k; j++) {
+                dp[0][2 * j + 1] =  - prices[0];
+            }
+        for (int i = 1; i < prices.length; i++) {
             for (int j = 0; j < k; j++) {
                 dp[i][2 * j + 1] = Math.max(dp[i - 1][2 * j + 1], dp[i - 1][2 * j] - prices[i]);
                 dp[i][2 * j + 2] = Math.max(dp[i - 1][2 * j + 2], dp[i - 1][2 * j + 1] + prices[i]);
             }
-        }
-        System.out.println(Arrays.deepToString(dp));
+        }      
         return dp[prices.length - 1][2 * k];
     }
 }
