80ce7263422967089f7aca9f237c0e92f59abc66
daydream <32980275+daydream1024@users.noreply.github.com>
2024-12-27 14:19:28 +0800

    feat(src): 添加新解决方案并创建新问题类
    - 在 No53.java 中添加了新的 Solution53Three 类，实现了动态规划算法
    - 新增 No583.java 文件，包含 Solution583 类和最小编辑距离问题的解决方案

--- src/No53.java
+++ src/No53.java
@@ -6,7 +6,7 @@
 public class No53 {
 }
 
-class Solution53 {
+class Solution53One {
     public int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int count = 0;
@@ -15,6 +15,19 @@ class Solution53 {
             if (nums[i] > count)
                 count = nums[i];
             max = Math.max(count, max);
+        }
+        return max;
+    }
+}
+
+//dp算法
+class Solution53Three {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length + 1];
+        int max = -10000;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
+            max = Math.max(dp[i], max);
         }
         return max;
     }
