f7697ba7d6702714c72053d5fa1fe39c47fb3e41
daydream <32980275+daydream1024@users.noreply.github.com>
2025-02-15 17:13:08 +0800

    feat(src): 添加No1207类以实现唯一出现次数的验证

--- src/No1365.java
+++ src/No1365.java
@@ -3,14 +3,14 @@ import java.util.HashMap;
 
 public class No1365 {
     public static void main(String[] args) {
-        Solution solution = new Solution();
+        Solution1365 solution = new Solution1365();
         int[] nums = {8, 1, 2, 2, 3};
         System.out.println(Arrays.toString(solution.smallerNumbersThanCurrent(nums)));
     }
 
 }
 
-class Solution {
+class Solution1365 {
     public int[] smallerNumbersThanCurrent(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] res = new int[nums.length];
